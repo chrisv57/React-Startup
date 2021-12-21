d72d6ab284b95ec1afedbb2357543d5da4a1eafa
chrisv57 <verma_c@outlook.com>
2021-12-21 15:24:32 -0500

    Imperative Approach

--- src/App.js
+++ src/App.js
@@ -75,19 +75,28 @@ const App = () => {
 };
 
 // Declarative Implementation of autofocus for the inputField
-const InputWithLabel = ({ id,label,value,type='text',onInputChange,isFocused,children }) => (
+const InputWithLabel = ({ id,label,value,type='text',onInputChange,isFocused,children }) => {
+  const inputRef = React.useRef();
+  React.useEffect(()=>{
+    if(isFocused && inputRef.current){
+      inputRef.current.focus();
+    }
+  },[isFocused]
+  );
+  return (
   <>
     <label htmlFor={id}>{children}</label>
     &nbsp;
     <input
+    ref={inputRef}
       id={id}
       type={type}
       value={value}
-      autoFocus={isFocused}
       onChange={onInputChange}
     />
   </>
-);
+  );
+  };
 
 //USing Spread and Rest Operators
 const List = ({ list }) => (
