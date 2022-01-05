f579f273ae323eba957b56fe795bd569f440ddf5
chrisv57 <verma_c@outlook.com>
2022-01-05 17:47:31 -0500

    Data Fetching

--- src/App.js
+++ src/App.js
@@ -12,32 +12,10 @@ const useSemiPersistentState = (key, initalState) => {
   }, [value, key]);
   return [value, setValue];
 };
-const initalStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initalStories } }),
-      2000
-    )
-  );
+
+
+
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -68,6 +46,10 @@ const storiesReducer = (state, action) => {
       throw new Error();
   }
 };
+
+//A
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
@@ -80,28 +62,25 @@ const App = () => {
     {data:[],isLoading:false,isError:false}
   );
 
-
-  // //Conditional Redenring: Setting a loading interface to let the users know about the loading of the data
-  // const [isLoading, setisLoading] = React.useState(false);
-
-  // const [isError, setisError] = React.useState(false);
-
   React.useEffect(() => {
+    if(!searchTerm) return;
     
     dispatchStories({type:'STORIES_FETCH_INIT'});
 
-    getAsyncStories()
-    .then(result => {
+    // Template Literal is used 
+    fetch(`${API_ENDPOINT}${searchTerm}`)  //B
+    .then((response) => response.json()) //C
+    .then((result) => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits, //D
       });
     })
 
       //Providing a catch to the promise in case something went wrong while fethcing data.
       .catch(() => dispatchStories({type: 'STORIES_FETCH_FAILURE'})
    );
-   }, []);
+   }, [searchTerm]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
@@ -147,7 +126,7 @@ always evaluates to false. */}
       {stories.isLoading ? (
         <p>Loading....</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
 
     </div>
