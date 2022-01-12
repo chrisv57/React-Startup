d0f83ebf230e114585156c4050b7c1ac48f01855
chrisv57 <verma_c@outlook.com>
2022-01-11 20:19:46 -0500

    Test Suites, Test Cases, and Assertions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+// The “describe” block is our test suite, and the “test” blocks are our test cases.
+
+describe('Something true and false',()=>{
+  test('true to be true',()=>{
+    expect(true).toBeTruthy();
+  });
+
+  test('false to be false',()=>{
+  expect(false).toBeFalsy();
+  });
 });
+
+
+//Test blocks can also be written as "it".
+// describe('App Component',()=>{
+//   it('removes an item when clicking the dismiss button',()=>{
+
+//   });
+
+//   it('request initial stoires from the API',()=>{
+
+//   });
+// });
